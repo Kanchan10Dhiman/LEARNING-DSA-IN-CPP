3a4fa942448acb00927ed2cdd5313fd990ae00db
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-03-18 15:08:36 +0530

    added

--- Conditional1/Program9.cpp
+++
@@ -1,6 +0,0 @@
-//Given three points (x1, y1), (x2, y2) and (x3, y3), write a program to check if all the three points fall on one straight line.
-#include<iostream>
-using namespace std;
-int main(){
-
-if(checkC
