122950eeb1e90fe46c1d592b3c6d416dabc69476
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-03-18 15:19:04 +0530

    added

--- SPECIAL QUESTION/lengthAndBreadth.cpp
+++ SPECIAL QUESTION/lengthAndBreadth.cpp
@@ -1,13 +1,18 @@
-//Given an integer Print the absolute value of that integer.
+//Given the length and breadth of a rectangle, write a program to find whether numerically the area of the rectangle is greater than its perimeter
 #include<iostream>
 using namespace std;
 int main(){
-    int a=-56;
-    if(a>0){
-        cout<<(a);
+    int length=20,breadth=10,area,perimeter;
+    area=length*breadth;
+    perimeter=2*(length+breadth);
+    if(area>perimeter){
+        cout<<"area is greater than the perimeter";
     }
-    else if(a<0){
-        cout<<(-a);
-    }
+    else {
+        cout<<"area is not greater";
+    } 
 
+return 0;
+
+    
 }
