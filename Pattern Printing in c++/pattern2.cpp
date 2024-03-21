2d42c6a5aae2cb6d71c8c7e448a4f19178049a5d
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-03-21 22:12:43 +0530

    add file

---
+++ Pattern Printing in c++/pattern2.cpp
@@ -0,0 +1,19 @@
+/*
+Print the Solid Square  pattern 
+****
+****
+**** 
+**** 
+
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=1;i<=4;i++){
+        for(j=1;j<=4;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
