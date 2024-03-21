1e090ac3468a6fa63cd5b3f522b2ceb655dd1616
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-03-21 18:21:39 +0530

    added file

---
+++ Loops in c++/Advanced Problems/problem6.cpp
@@ -0,0 +1,16 @@
+//WAP to print sum of digits of a given number.
+#include<iostream>
+using namespace std;
+int main(){
+    int n,b,sum=0;
+    cout<<"enter the number n";
+    cin>>n;
+    while(n>1){
+        b=n%10;
+        sum=sum+b;
+        n=n/10;
+
+    }
+    cout<<sum;
+
+}
