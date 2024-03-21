aea714f2b6018359d8484457a22c7879fff2942a
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-03-21 07:23:11 +0530

    added new file

--- Loops in c++/mediumLevelProblem2.cpp
+++ Loops in c++/mediumLevelProblem2.cpp
@@ -1,1 +1,17 @@
 //Display this GP - 1,2,4,8,16,32,.. upto ‘n’ terms.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of n";
+    cin>>n;
+    int i=1;
+
+     do{
+        cout<<i<<",";
+        i=i+i;
+    }
+    while(i<n);
+    return 0;
+
+}
