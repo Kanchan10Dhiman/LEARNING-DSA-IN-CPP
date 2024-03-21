3f08f678084b51458ba2609c193d0dd772e5a1cc
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-03-21 22:33:13 +0530

    add file

---
+++ Pattern Printing in c++/pattern4.cpp
@@ -0,0 +1,20 @@
+/*
+Print the star triangle pattern
+*
+**
+***
+****
+
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=1;i<=4;i++){
+        for(j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
