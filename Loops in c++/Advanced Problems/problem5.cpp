2ebba15b32090f53b3b731c767ce975180b8eff1
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-03-21 17:46:21 +0530

    added file

---
+++ Loops in c++/Advanced Problems/problem5.cpp
@@ -0,0 +1,18 @@
+//WAP to check if a number is prime or not (use of break statement).
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i;
+    cout<<"enter the number";
+    cin>>n;
+    for(i=2;i<=n-1;i++){
+        if(n%i==0)
+         break;
+    }
+    if(i==n){
+        cout<<"is prime number";
+    }
+    else{
+        cout<<"not a prime number";
+    }
+}
