3ccc9066d35629f90d2840da45c133867e0ecdff
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-03-22 09:07:44 +0530

    add file

---
+++ Pattern Printing in c++/pattern7.cpp
@@ -0,0 +1,17 @@
+//PRINT THE STAR CROSS PATTERN
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=5;j++){
+            if(i==j || j==6-i){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
