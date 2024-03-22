bc0130037a30fa61f5ecc62443e3656235165c0a
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-03-22 11:42:41 +0530

    add file

---
+++ Pattern Printing in c++/pattern11.cpp
@@ -0,0 +1,19 @@
+/*
+Print the given pattern
+Number Triangle Flipped
+ 1
+ 1 2
+ 1 2 3
+ 1 2 3 4
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=1;i<=4;i++){
+        for(j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
