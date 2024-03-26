c77eac027e46cb60c32e4c35aac1fcc1c8831dca
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-03-26 22:58:30 +0530

    add file

---
+++ Functions in c++/function1.cpp
@@ -0,0 +1,18 @@
+//Write a function that takes the radius of a circle as an argument and returns its area.
+
+#include <iostream>
+using namespace std;
+float area(float r){
+    return  3.14*r*r;
+}
+int main(){
+    float radius;
+    cout<<"Enter  radius: ";
+    cin>>radius;
+
+    float areaof = area(radius);                               
+    cout << "The area of circle is : " << areaof;
+    return 0;
+}
+
+
