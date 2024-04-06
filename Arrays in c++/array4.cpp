ef6ab848f7d77fa2a89ae8e005e5c619d9dc447a
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-04-06 13:35:31 +0530

    add file

---
+++ Arrays in c++/array4.cpp
@@ -0,0 +1,29 @@
+//Find the element x in the array . Take array and x as input.
+#include<iostream>
+using namespace std;
+int main(){
+    int array[20];
+    int i,d;
+    int x;
+    cout<<"enter array :";
+    for(int i=0;i<=5;i++){
+        cin>>array[i];//array as input
+        
+    }
+    cout<<"enter x:";
+    cin>>x;
+    for(i=0;i<=5;i++){
+    
+    if(array[i]==x){
+        d=1;
+    }}
+
+    if(d==1){
+        cout<<"found";
+    }
+    else{
+        cout<<"notfound";
+    }
+    
+    
+}
