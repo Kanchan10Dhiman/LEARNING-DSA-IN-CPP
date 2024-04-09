f28b9f830b7fe8614cca0b5374fc09e9974fd3c5
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-04-09 19:59:41 +0530

    add file

---
+++ 2D arrays in c++/2darray2.cpp
@@ -0,0 +1,31 @@
+//Write a program to add two matrices.
+#include<iostream>
+using namespace std;
+int main(){
+    int array1[2][2],array2[2][2],sum[2][2],i,j;
+    cout<<"enter 1st matrix";
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            cin>>array1[i][j];
+        }
+    }
+    cout<<"enter 2nd matrix";
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            cin>>array2[i][j];
+        }
+    }
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            sum[i][j]=array1[i][j]+array2[i][j];
+        }
+    }
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            cout<<sum[i][j];
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
