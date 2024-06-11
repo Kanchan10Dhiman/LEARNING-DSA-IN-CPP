cccd26a02eef04461ffb85741f2b7244d13c178e
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-06-11 11:39:48 +0530

    add changes

--- Loops in c++/Advanced Problems/problem8.cpp
+++ Loops in c++/Advanced Problems/problem8.cpp
@@ -17,3 +17,22 @@ int main(){
         cout<<"not prime";
      return 0;
 }
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    bool flag=true; 
+    for(int i=2; i<=n-1; i++){
+        if(n%i==0){
+            flag = false;
+            break;
+
+        }
+    }   
+    if(n==1) cout<<" 1 is neither prime nor composite";
+    if(flag==true) cout<<"its a prime number";
+    else cout<<n<<" is a composite number";
+}*/
