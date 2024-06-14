095446f3e9e4ceef8e8331ccfc59b77d37c20472
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-06-14 17:11:19 +0530

    add file

--- Loops in c++/Advanced Problems/problem13.cpp
+++ Loops in c++/Advanced Problems/problem13.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 int main(){
     int n,i,a=0,b=1,c;
+    cout<<"enter nth term";
     cin>>n;
     for(i=1;i<=n;i++){
+        cout<<a<<",";
         c=a+b;
         a=b;
         b=c;
-        cout<<a<<",";
     }
     return 0;
 }
