d71bf3f73c37dc39ffe56a065c5d9fb138e735b2
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2024-06-14 16:59:20 +0530

    add file

---
+++ Loops in c++/Advanced Problems/problem13.cpp
@@ -0,0 +1,14 @@
+//Print the nth fibonacci number.
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,a=0,b=1,c;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+        cout<<a<<",";
+    }
+    return 0;
+}
